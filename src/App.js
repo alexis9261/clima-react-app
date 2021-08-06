96d531727ecdc1caf5401b71c964ea536da6527a
Alexis Montilla <alexisamm9261@gmail.com>
2021-08-06 16:19:42 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Clima from './components/Clima';
+import Error from './components/Error';
 
 function App() {
+
+  // State que guarda el resultado de la busqueda
+  const [ busqueda, setBusqueda ] = useState({
+    ciudad:'',
+    pais:''
+  });
+
+  // State bandera que indica que se realizao la consulta
+  const [ consulta, setConsulta ] = useState(false);
+  const [ resultado, setResultado ] = useState({});
+  const [ error, setError ] = useState(false);
+
+  const { ciudad, pais } = busqueda
+
+  useEffect( () => {
+    const consultarAPI = async () => {
+
+      if(consulta){
+
+        const appID = 'ce8e3b25afd3a13f8ef8b2be362f2427';
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appID}`;
+        
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        
+        setResultado(resultado);
+        setConsulta(false);
+
+        // detectar si hubo respuesta correcta en la consulta
+        if(resultado.cod === "404"){
+          setError(true);
+        }else{
+          setError(false);
+        }
+      }
+
+    }
+
+    consultarAPI();
+
+    // Para deshabilitar las dependencias se coloca el siguiente comentario al final del useEffect
+    // eslint-disable-next-line
+  }, [consulta]);
+
+
+  // CARGA CONDICIONAL DE COMPONENTES
+  let componente;
+  if(error){
+    componente = <Error mensaje="No hay resulados para esta búsqueda" />
+  }else{
+    componente = <Clima resultado={resultado} />
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment >
+      <Header 
+        titulo={'Clima React App'}
+      />
+
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario 
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsulta={setConsulta}
+              />
+            </div>
+            <div className="col m6 s12">
+              {componente}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
+// Client ID
+// ce8e3b25afd3a13f8ef8b2be362f2427
+
 export default App;
+
---
+++ src/components/Formulario.js
@@ -0,0 +1,90 @@
+import React, {useState} from 'react';
+import Error from './Error';
+import PropTypes from 'prop-types';
+
+const Formulario = ({ busqueda, setBusqueda, setConsulta }) => {
+
+    const [ error, setError ] = useState(false);
+
+    // extraer ciudad y pais del state
+    const { ciudad, pais } = busqueda;
+
+    // funcionque coloca los elementos en el state
+    const handleChange = event => {
+        // actualizar el state
+        setBusqueda({
+            ...busqueda,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    // formulario submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // validar
+        if( ciudad.trim() === '' || pais.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+
+        // enviar la info al componente principal
+        setConsulta(true);
+
+    }
+
+    return ( 
+        <form
+            onSubmit={handleSubmit}
+        >
+            { error ? <Error mensaje="Todos los campos son obligatorios" /> : null }
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
+                />
+                <label htmlFor="ciudad">Ciudad:</label>
+            </div>
+            <div className="input-field col s12">
+                <select
+                    name="pais"
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                >
+                    <option value="">-- Selecciona un país --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="VE">Venezuela</option>
+                </select>
+                <label htmlFor="pais">País:</label>
+            </div>
+            <div className="input-flied col s12">
+                <input 
+                    type="submit"
+                    value="Buscar clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
+
+            </div>
+        </form>
+     );
+}
+
+// documentar el componente
+Formulario.propTypes = {
+    busqueda: PropTypes.object.isRequired,
+    setBusqueda: PropTypes.func.isRequired,
+    setConsulta: PropTypes.func.isRequired
+}
+ 
+export default Formulario;
